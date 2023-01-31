21a2be0195e4440b9004716dd290b76e9a2679af
OGhantous <122539542+OGhantous@users.noreply.github.com>
2023-01-31 10:07:17 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
-input.onGesture(Gesture.Shake, function () {
-    Reset()
-})
-function Reset () {
-    let Rounds = 0
-    let Ties = 0
-    let P2 = 0
-    let P1 = 0
-    OLED.init(128, 64)
-    OLED.writeStringNewLine("Shall we play a game")
-    basic.pause(2000)
+function updateScoreboard () {
     OLED.clear()
     OLED.writeStringNewLine("Player 1 score:" + P1)
     OLED.newLine()
@@ -18,4 +8,39 @@ function Reset () {
     OLED.newLine()
     OLED.writeStringNewLine("Rounds:" + Rounds)
 }
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    P1 += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("T")
+    Ties += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    P2 += 1
+    Rounds += 1
+    updateScoreboard()
+})
+input.onGesture(Gesture.Shake, function () {
+    Reset()
+})
+function Reset () {
+    OLED.init(128, 64)
+    Rounds = 0
+    P2 = 0
+    P1 = 0
+    Ties = 0
+    OLED.writeStringNewLine("Shall we play a game")
+    basic.pause(2000)
+    updateScoreboard()
+}
+let Rounds = 0
+let Ties = 0
+let P2 = 0
+let P1 = 0
 Reset()
